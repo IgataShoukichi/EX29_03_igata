c4402b0ed75340aea05f929286760ef6ef835afd
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2022-07-20 17:12:55 +0900

    内容変更

--- Program.cs
+++ Program.cs
@@ -5,9 +5,37 @@ namespace Ex_2022_Template
     {
         static void Main(string[] args)
         {
+            Box.box=new Box(
+                InputUtility.InputFloat("幅"),
+                InputUtility.InputFloat("高さ"),
+                InputUtility.InputFloat("奥行"));
+            Console.WriteLine($"Boxの表面積={Box.GetSurface()}boxの体積{Box.GetVolume()}");
+        }
+        {
             var name = InputUtility.Input("Name:");
             var age = InputUtility.InputNumber("Age:", 120);
             Console.WriteLine($"Hello {name}! \n U are {age} yeaes old.");
         }
+    }
+    class Box
+    {
+        private float width;
+        private float height;
+        private float depth;
+
+        public Box(float width,float height,float depth)
+        {
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+        }
+        public float GetSurface()
+        {
+            return (width*height+depth*height+width*depth)*2;
+        }
+        public float GetVolume()
+    {
+        return width*height*depth;
+    }
     }
 }
